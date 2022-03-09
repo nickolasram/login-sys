9323d1ced00fbce8f7dc5eab7742698c90456d9e
Nickolas <91910852+nickolasram@users.noreply.github.com>
2022-03-08 18:17:02 -0800

    Update second.py
    Added slashes to link addresses

--- second.py
+++ second.py
@@ -3,14 +3,14 @@ from user.models import User
 
 second = Blueprint("second", __name__, static_folder="static", template_folder="templates")
 
-@second.route('/signup', methods=['POST'])
+@second.route('/signup/', methods=['POST'])
 def signup():
   return User().signup()
 
-@second.route('/signout')
+@second.route('/signout/')
 def signout():
   return User().signout()
 
-@second.route('/login', methods=['POST'])
+@second.route('/login/', methods=['POST'])
 def login():
   return User().login()
